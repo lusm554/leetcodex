bcaa53a7ea84bb0332881813eb3e0617ead94274
lusm <loveyousomuch554@gmail.com>
2025-07-30 12:31:00 +0300

    group-sold-products-by-the-date

---
+++ sql50/group-sold-products-by-the-date.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+select
+    sell_date,
+    count(distinct product) num_sold,
+    string_agg(distinct product, ',' order by product) products
+from Activities
+group by 1
+order by sell_date
+;
